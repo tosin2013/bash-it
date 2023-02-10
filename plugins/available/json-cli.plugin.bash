87cd6cfc06e21f3677da0568401a369e7c4cab44
Tosin Akinosho <tosin.akinosho@gmail.com>
2023-02-10 17:22:58 -0500

    adding php install for json-cli

--- plugins/available/json-cli.plugin.bash
+++ plugins/available/json-cli.plugin.bash
@@ -6,10 +6,30 @@ about-plugin 'A CLI app to find unordered diff between two JSON documents (based
 
 if ! _command_exists json-cli
 then
+  function php-install() {
+      # Check the Linux distribution
+      distribution=$(cat  /etc/os-release | grep ID | head -1 | cut -d "=" -f 2)
+      # Install PHP based on the Linux distribution
+      if [ "$distribution" == "ubuntu" ] || [ "$distribution" == "debain" ]; then
+        sudo apt-get update -y
+        sudo apt-get install -y php
+      elif [ "$distribution" == "centos" ] || [ "$distribution" == "rhel" ]|| [ "$distribution" == "rocky" ]; then
+        sudo dnf update -y
+        sudo dnf install -y php
+      else
+        echo "Error: unsupported Linux distribution: $distribution"
+        exit 1
+      fi
+  }
+
   function json-cli-install() {
-   curl -OL https://github.com/swaggest/json-cli/releases/download/v1.11.1/json-cli
-   chmod +x json-cli
-   sudo mv json-cli /usr/local/bin/json-cli
+    curl -OL https://github.com/swaggest/json-cli/releases/download/v1.11.1/json-cli
+    chmod +x json-cli
+    sudo mv json-cli /usr/local/bin/json-cli
+    if ! _command_exists php
+    then
+      php-install
+    fi
   }
 
   json-cli-install
